bc46ab336536808efc513f6a08e823608c7e3cc5
KM <18610929200@163.com>
2018-11-14 15:25:39 +0800

    pretty cell xib

--- Douyu/Classes/Home/Controller/RecommandViewController.swift
+++ Douyu/Classes/Home/Controller/RecommandViewController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+
 private let kItemMargin: CGFloat = 10
 private let kItemW = (kScreenW - 3 * kItemMargin) / 2
-private let kItemH = kItemW * 3 / 4
+private let kNormalItemH = kItemW * 3 / 4
+private let kPrettyItemH = kItemW * 4 / 3
 private let kNormalCellID = "kCellIdentify"
+private let kPrettyCell = "kPrettyCell"
 private let kHeaderViewID = "kHeaderViewID"
 class RecommandViewController: UIViewController {
 
@@ -18,7 +21,7 @@ class RecommandViewController: UIViewController {
     lazy var collectionView: UICollectionView = {[unowned self] in
         
         let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: kItemW, height: kItemH)
+        layout.itemSize = CGSize(width: kItemW, height: kNormalItemH)
         layout.minimumLineSpacing = 0
         layout.minimumInteritemSpacing = kItemMargin
         layout.headerReferenceSize = CGSize(width: kScreenW, height: 50)
@@ -27,10 +30,13 @@ class RecommandViewController: UIViewController {
         print(self.view.bounds)
         let collectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: layout)
         collectionView.dataSource = self
+        collectionView.delegate = self
         collectionView.autoresizingMask = [.flexibleWidth,.flexibleHeight]
         collectionView.backgroundColor = UIColor.white
         
         collectionView.register(UINib(nibName: "CollectionNormalCell", bundle: nil), forCellWithReuseIdentifier: kNormalCellID)
+        collectionView.register(UINib(nibName: "CollectionPrettyCell", bundle: nil), forCellWithReuseIdentifier: kPrettyCell)
+        
         collectionView.register(UINib(nibName: "CollectionReusableView", bundle: nil), forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: kHeaderViewID)
 
         return collectionView
@@ -41,6 +47,8 @@ class RecommandViewController: UIViewController {
         view.backgroundColor = UIColor.orange
         
         setupUI()
+        
+        getHomeData()
     }
 
 }
@@ -49,9 +57,14 @@ extension RecommandViewController {
     func setupUI() {
         view.addSubview(collectionView)
     }
+    
+    func getHomeData() {
+        
+    }
+    
 }
-//MARK: -Delegate
-extension RecommandViewController: UICollectionViewDataSource {
+//MARK: -UICollectionViewDataSource
+extension RecommandViewController: UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 12
     }
@@ -61,8 +74,24 @@ extension RecommandViewController: UICollectionViewDataSource {
         }
         return 4
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if indexPath.section == 1 {
+            return CGSize(width: kItemW, height: kPrettyItemH)
+        }else {
+            return CGSize(width: kItemW, height: kNormalItemH)
+        }
+    }
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kNormalCellID, for: indexPath)
+        
+        var cell: UICollectionViewCell
+        
+        if indexPath.section == 1 {
+            cell = collectionView.dequeueReusableCell(withReuseIdentifier: kPrettyCell, for: indexPath)
+        }else {
+            cell = collectionView.dequeueReusableCell(withReuseIdentifier: kNormalCellID, for: indexPath)
+        }
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
---
+++ Douyu/Classes/Main/View/CollectionPrettyCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionPrettyCell.swift
+//  Douyu
+//
+//  Created by Roc01 on 2018/11/13.
+//  Copyright © 2018 Roc.iMac01. All rights reserved.
+//
+
+import UIKit
+
+class CollectionPrettyCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
