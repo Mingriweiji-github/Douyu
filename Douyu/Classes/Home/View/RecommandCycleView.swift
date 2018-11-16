77c3f7b22228b3503713b0699577b4fbc99f039c
KM <18610929200@163.com>
2018-11-16 18:16:20 +0800

    监听代理方法,手动拖拽

--- Douyu/Classes/Home/View/RecommandCycleView.swift
+++ Douyu/Classes/Home/View/RecommandCycleView.swift
@@ -10,14 +10,19 @@ import UIKit
 private var kCycleCellID = "kCycleCellID"
 class RecommandCycleView: UIView {
     
+    var cycleTime: Timer?
+    
     var cycleModels: [CycleModel] = [] {
         didSet {
             collectionView.reloadData()
-            pageControl.numberOfPages = cycleModels.count ?? 0
+            pageControl.numberOfPages = cycleModels.count
             
             //默认位置为600,解决第一张左滑的问题
-            let indexPath = IndexPath(item: (cycleModels.count ?? 0) * 100, section: 0)
+            let indexPath = IndexPath(item: (cycleModels.count ) * 100, section: 0)
             collectionView.scrollToItem(at: indexPath, at: .left, animated: false)
+            //解决自动滑动
+            removeTimer()
+            addTimer()
         }
     }
 
@@ -70,9 +75,31 @@ extension RecommandCycleView: UICollectionViewDataSource,UICollectionViewDelegat
     //mark: UICollectionViewDelegate
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let offsetX = scrollView.contentOffset.x + scrollView.bounds.width * 0.5
-        
         //取模解决最后一张右滑问题
         pageControl.currentPage = Int(offsetX / kScreenW) % (cycleModels.count ?? 1)
     }
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        removeTimer()
+    }
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        addTimer()
+    }
     
+}
+
+extension RecommandCycleView {
+    func addTimer() {
+        cycleTime = Timer(timeInterval: 2.0, target: self, selector: #selector(scrollNext), userInfo: nil, repeats: true)
+        RunLoop.main.add(cycleTime!, forMode: .commonModes)
+    }
+    func removeTimer() {
+        cycleTime?.invalidate()
+        cycleTime = nil
+    }
+    func scrollNext() {
+        let collectionX = collectionView.contentOffset.x
+        let offSetX = collectionX + collectionView.bounds.width
+        
+        collectionView.setContentOffset(CGPoint(x: offSetX, y: 0), animated: true)
+    }
 }
